ff79a97c86d894075910b2db80c488c42d39af54
adriannic <nicolas.aguilera.adrian@gmail.com>
2025-09-10 18:31:54 +0200

    feat: change to animeflv.one

--- src/scraper/animeflvscraper.rs
+++ src/scraper/animeflvscraper.rs
@@ -10,14 +10,14 @@ pub struct AnimeFlvScraper;
 
 impl Scraper for AnimeFlvScraper {
     fn try_search(client: &Client) -> Result<Vec<Anime>, Box<dyn Error>> {
-        let pages = 150;
+        let pages = 300;
         let anime_re = Regex::new(
-            r#"<article class="Anime alt B">[\s\S]*?"\/anime\/(.*?)"[\s\S]*?(\d+)\.jpg[\s\S]*?Title">(.*?)<[\s\S]*?<\/p>[\s\S]*?<p>([\s\S]*?)<\/p>[\s\S]*?<\/article>"#,
+            r#"<article class="li">[\s\S]*?<figure class="i">[\s\S]*?<a href=".\/anime\/(.*?)" title="Ver Anime (.*?) Online Gratis">[\s\S]*?data-src="(.*?)\?[\s\S]*?<\/article>"#,
         )?;
 
         let urls = (1..=pages)
             .into_par_iter()
-            .map(|page| format!("https://www3.animeflv.net/browse?page={page}"))
+            .map(|page| format!("https://vww.animeflv.one/animes?pag={page}"))
             .collect::<Vec<_>>();
 
         let bodies = urls
@@ -30,13 +30,12 @@ impl Scraper for AnimeFlvScraper {
             .par_bridge()
             .filter_map(|c| {
                 let slug = c.get(1)?.as_str();
-                let id = c.get(2)?.as_str();
-                let title = c.get(3)?.as_str();
-                let synopsis = c.get(4)?.as_str();
+                let title = c.get(2)?.as_str();
+                let img = c.get(3)?.as_str();
                 Some(Anime {
                     names: vec![title.into(), slug.into()],
-                    synopsis: synopsis.into(),
-                    image_url: format!("https://www3.animeflv.net/uploads/animes/covers/{id}.jpg"),
+                    synopsis: title.into(),
+                    image_url: img.into(),
                 })
             })
             .collect::<Vec<_>>();
@@ -47,32 +46,22 @@ impl Scraper for AnimeFlvScraper {
     }
 
     fn try_get_episodes(client: &Client, slug: &str) -> Result<Vec<f64>, Box<dyn Error>> {
-        let pattern = Regex::new("var episodes = .*?;")?;
+        let episodes_re = Regex::new(r#"\["(.*?)",[\s\S]*?"0",[\s\S]*?""\]"#)?;
         let anime = client
-            .get(format!("https://www3.animeflv.net/anime/{slug}"))
+            .get(format!("https://vww.animeflv.one/anime/{slug}"))
             .send()
             .unwrap()
             .text()
             .unwrap();
 
-        let binding = pattern
-            .find(&anime)
-            .ok_or("episodes variable not found")?
-            .as_str()
-            .split_whitespace()
-            .skip(3)
-            .join("");
+        let mut episodes = episodes_re
+            .captures_iter(&anime)
+            .par_bridge()
+            .filter_map(|c| c.get(1))
+            .filter_map(|m| m.as_str().parse::<f64>().ok())
+            .collect::<Vec<_>>();
 
-        let episodes = binding
-            .strip_prefix("[[")
-            .unwrap()
-            .strip_suffix("]];")
-            .unwrap()
-            .split("],[")
-            .filter_map(|ep| ep.split(',').next())
-            .filter_map(|ep| ep.parse::<f64>().ok())
-            .sorted_by(|a, b| a.partial_cmp(b).unwrap())
-            .collect_vec();
+        episodes.sort_by(|a, b| a.partial_cmp(b).unwrap());
 
         Ok(episodes)
     }
@@ -83,17 +72,23 @@ impl Scraper for AnimeFlvScraper {
         episode: f64,
     ) -> Result<Vec<String>, Box<dyn Error>> {
         let response = client
-            .get(format!("https://www3.animeflv.net/ver/{slug}-{episode}"))
+            .get(format!("https://vww.animeflv.one/ver/{slug}-{episode}"))
             .send()
             .unwrap()
             .text()
             .unwrap();
 
-        let pattern = Regex::new("\"code\":\".*?\"")?;
+        let pattern = Regex::new(r#"(www\.mp4upload.com\\\/.*?)&quot"#)?;
 
         let mirrors = pattern
-            .find_iter(&response)
-            .map(|found| found.as_str().to_owned().replace(['\\', '"'], "")[5..].to_owned())
+            .captures_iter(&response)
+            .filter_map(|c| c.get(1))
+            .map(|found| {
+                let [first, second] = found.as_str().split("\\/").collect_vec()[..2] else {
+                    panic!("Shouldn't happen")
+                };
+                format!("https://{}/embed-{}.html", first, second)
+            })
             .collect_vec();
 
         Ok(mirrors)
